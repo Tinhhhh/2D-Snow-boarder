84a4f86a15fb4a8bd7e300515fe8adaad21338b1
Tinhhhh <tinhvvse173112@fpt.edu.vn>
2024-07-04 16:26:24 +0700

    first commit

---
+++ Assets/Script/Controller.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField] private float torqueValue = 5f;
+
+    private Rigidbody2D rb;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            rb.AddTorque(torqueValue);
+        }
+
+         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            rb.AddTorque(torqueValue);
+        }
+    }
+}
