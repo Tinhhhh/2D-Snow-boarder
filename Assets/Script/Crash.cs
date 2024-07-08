ab1f54a3be4c79cc7a1a763c307555fc82a16ccd
Tinhhhh <tinhvvse173112@fpt.edu.vn>
2024-07-08 11:00:59 +0700

    add Crash

---
+++ Assets/Script/Crash.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crash : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Ground")
+        {
+            Debug.Log("Player has crashed!");
+        }
+        
+    }
+}
